d6a804aa6b5767ad9dd78b99c8425401723071cc
Cheikhna ABDEL WEDOUD <Cheikhna.ABDELWEDOUD@gmail.com>
2019-10-31 09:12:45 +0100

    Add exit and input check for the launcher

--- gearbox/src/main/java/electrics/industries/launcher/Launcher.java
+++ gearbox/src/main/java/electrics/industries/launcher/Launcher.java
@@ -11,11 +11,17 @@ public class Launcher {
 		Scanner scan = new Scanner(System.in);
 		while (true) {
 
-			System.out.println("Please enter the engine speed : ");
-			int engineSpeed = scan.nextInt();
-			gearbox.calculateSpeed(engineSpeed);
-			System.out.println("Speed: " + gearbox.getSpeed());
-			System.out.println("Engine speed: " + gearbox.getCurrentEngineSpeed() + "\n");
+			System.out.println("Please enter the engine speed or X if you want to stop:");
+			if (scan.hasNextInt()) {
+				int engineSpeed = scan.nextInt();
+				gearbox.calculateSpeed(engineSpeed);
+				System.out.println("Speed: " + gearbox.getSpeed());
+				System.out.println("Engine speed: " + gearbox.getCurrentEngineSpeed() + "\n");
+			} else if ("X".equalsIgnoreCase(scan.nextLine())) {
+				scan.close();
+				System.out.println("Gearbox calculator stopped!");
+				System.exit(0);
+			}
 		}
 	}
 }
